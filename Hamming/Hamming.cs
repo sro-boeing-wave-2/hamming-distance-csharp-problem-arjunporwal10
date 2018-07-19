f34d018b728581fa4c39c99bc708b06eef891e33
arjun <arjunporwal10@gmail.com>
2018-07-19 10:25:42 +0530

    All test cases passed

--- Hamming/Hamming.cs
+++ Hamming/Hamming.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Hamming
 {
@@ -7,8 +8,27 @@ namespace Hamming
         public static int Distance(string original, string current)
         {
             int hammingDistance = 0;
-            
+            if (current == null)
+            {
+                throw new ArgumentNullException("current");
+            }
+            if (original == null)
+            {
+                throw new ArgumentNullException("original");
+            }
+            if (original.Length != current.Length)
+            {
+                throw new ArgumentException("Hamming Distance can only be calculated over strings of equal length");
+            }
+            char[] charOriginal = original.ToArray();
+            char[] charCurrent = current.ToArray();
+            for (int i = 0; i < charOriginal.Length; i++)
+            {
+                if (charOriginal[i] != charCurrent[i])
+                    hammingDistance++;
+            }
             return hammingDistance;
+            
         }
     }
 }
